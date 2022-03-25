56774788534f421e818ee496df648d59d68d7e63
Kadobkina EA <kadobkina.ea@mail.ru>
2022-03-26 00:52:54 +0300

    add xfail for one test

--- pages/product_page.py
+++ pages/product_page.py
@@ -7,13 +7,21 @@ class ProductPage(BasePage):
         
     def add_product_to_basket(self):
         button = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
-        button.click()
+        button.click()       
         
-    def correct_product_added_to_basket(self):
+    def should_be_success_message(self):
         alert = self.browser.find_element(*ProductPageLocators.SUCCESSFUL_ALERT)
         assert alert, "The product was not added to the basket"
+        
+    def should_be_correct_product_in_basket(self):
+        product_name_in_alert = self.browser.find_element(*ProductPageLocators.SUCCESSFUL_ALERT)
         product_name_in_page = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
-        assert product_name_in_page.text == alert.text, "A product with a different name has been added to the basket"
-        product_price_in_page = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
-        product_price_in_alert = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_ALERT)
+        assert product_name_in_page.text == product_name_in_alert.text, "A product with a different name has been added to the basket"
+        
+    def should_be_correct_price_in_basket(self):
+        product_price_in_page = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)  
+        product_price_in_alert = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_ALERT) 
         assert product_price_in_page.text == product_price_in_alert.text, "The price of the product does not match the price in the basket"
+        
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
--- test_product_page.py
+++ test_product_page.py
@@ -3,13 +3,14 @@ from .pages.product_page import ProductPage
 import time
 import pytest
 
-links = [f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{no}" for no in range(10)]
-
-@pytest.mark.parametrize('link', links)
-def test_guest_can_add_product_to_basket(browser, link):
+@pytest.mark.parametrize('promoN', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9", ])
+def test_guest_can_add_product_to_basket(browser, promoN):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promoN}"
     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()
     page.should_be_basket_button() # проверяем есть ли кнопка добавления в корзину
     page.add_product_to_basket() # добавляем товар в корзину
     page.solve_quiz_and_get_code() # решаем выражение
-    page.correct_product_added_to_basket() # верный ли продукт добавлен в корзину
+    page.should_be_success_message() # товар добавлен в корзину
+    page.should_be_correct_product_in_basket() # верный товар в корзине
+    page.should_be_correct_price_in_basket() # цена товара совпадает с ценой в корзине
