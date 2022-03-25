d6b017e7df1bc449defcd6c97268a7b7e8c9c199
Kadobkina EA <kadobkina.ea@mail.ru>
2022-03-25 03:35:13 +0300

    crate login page test

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,7 @@
-from .base_page import BasePage
-from selenium.webdriver.common.by import By
-    
-class MainPage(BasePage):
+from .pages.main_page import MainPage
 
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click() 
-        
-    
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
